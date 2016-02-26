697cc5f9f77f47b14149f9f6e79dbc8817b2546b
Pulat Mat <pulatmat@hotmail.com>
2016-02-26 09:45:54 -0500

    pulat test shared Git

--- Stored Procedures/dbo.CustOrdersDetail.sql
+++ Stored Procedures/dbo.CustOrdersDetail.sql
@@ -1,15 +1,18 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
 
-CREATE PROCEDURE [dbo].[CustOrdersDetail] @OrderID int
+
+CREATE PROCEDURE [dbo].[CustOrdersDetail] @OrderID INT
 AS
 SELECT ProductName,
     UnitPrice=ROUND(Od.UnitPrice, 2),
     Quantity,
-    Discount=CONVERT(int, Discount * 100), 
-    ExtendedPrice=ROUND(CONVERT(money, Quantity * (1 - Discount) * Od.UnitPrice), 2)
+    Discount=CONVERT(INT, Discount * 100), 
+    ExtendedPrice=ROUND(CONVERT(MONEY, Quantity * (1 - Discount) * Od.UnitPrice), 2)
 FROM Products P, [Order Details] Od
-WHERE Od.ProductID = P.ProductID and Od.OrderID = @OrderID
+WHERE Od.ProductID = P.ProductID AND Od.OrderID = @OrderID
+-- boing 747
 GO
