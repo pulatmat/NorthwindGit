e782e914b64af69ee7642fd03644c857e200a6b7
Pulat Mat <pulatmat@hotmail.com>
2016-03-01 10:53:39 -0500

    test from pulat 3/1/16

--- Views/dbo.Alphabetical list of products.sql
+++ Views/dbo.Alphabetical list of products.sql
@@ -1,10 +1,13 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
 
-create view [dbo].[Alphabetical list of products] AS
+
+CREATE VIEW [dbo].[Alphabetical list of products] AS
 SELECT Products.*, Categories.CategoryName
 FROM Categories INNER JOIN Products ON Categories.CategoryID = Products.CategoryID
 WHERE (((Products.Discontinued)=0))
+-- test from pulat 3/1/16
 GO
